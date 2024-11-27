d2f696dcd0da3ae217f1df1391a3fe9abdb8e889
Anthony CARRAUD <a.carraud@gmail.com>
2024-11-27 16:49:04 +0000

    Affichage Fonctionnel

--- script/script.js
+++ script/script.js
@@ -1,29 +1,76 @@
-let searchButton = document.getElementById('searchBut')
-searchByingredients();
+let searchButton = document.getElementById('searchBut');
+let ingrR = document.getElementById('ingR');
+let cockR = document.getElementById('cockR');
+let form = document.getElementById('form');
+let text = document.getElementById('searchChamps');
+let shakeM = document.getElementById('shakebutton');
+let cardConteneur = document.getElementById('cocktailsConteneur');
+
+let cocktByName = new Array();
+let cockByIngre = new Array();
 
 
+const API_BASE = "https://www.thecocktaildb.com/api/json/v1/1/";
 
-function searchByingredients(event){
+shakeM.addEventListener('click', shakeMeRandomCoktail)
+
+form.addEventListener("submit", click);
+function click(event) {
   event.preventDefault();
-  searchButton.addEventListener('click', () => {
-    const query = searchInput.value.trim();
-    const searchType = searchByName.checked ? 'name' : 'ingredient';
-    if (!query) return alert("Veuillez entrer un terme de recherche.");
-    const endpoint = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-      searchType === 'name'
-        ? `${API_BASE}search.php?s=${query}`
-        : `${API_BASE}filter.php?i=${query}`;
+  const query = text.value.trim();
+  console.log(text.value);
+
+  if (ingrR.checked == true && text.value !== "") {
+    console.log("Vous avez choisi la recherche par ingredient");
+    let endpoint =
+      `${API_BASE}filter.php?i=${query}`;
     fetch(endpoint)
       .then((response) => response.json())
-      .then((data) => displayResults(data.drinks))
+      .then((data) => AfficheCByCName(data.drinks))
       .catch(console.error);
-      
-  });
+    console.log(endpoint);
+
+  }
+  else if (ingrR.checked == false && cockR.checked == false) {
+    alert("Vous n'avez pas coché quel type de recherche vous souhaitiez faire, par nom de cocktail ou par ingrédient ?");
+  }
+  else if (text.value == null || text.value == undefined || text.value === "") {
+    alert("Vous n'avez rien entré");
+  }
+  else {
+    console.log("Vous avez choisi la recherche par Nom de Cocktail");
+
+    let endpoint =
+      `${API_BASE}search.php?s=${query}`
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => AfficheCByCName(data.drinks))
+      .catch(console.error);
+    console.log(endpoint);
+  }
 }
-function searchByCocktailName (){
-  preventDefault();
+
+
+function shakeMeRandomCoktail() {
+  console.log("Vous avez choisi la fonction random cocktail");
+}
+function createCoktail(){
 
 }
-function shakeMeRandomCoktail () {
-  preventDefault();
+
+
+
+function AfficheCByCName(drinks) {
+  cardConteneur.innerHTML="";
+  drinks.forEach(_cocktail => {
+    console.log(cardConteneur);
+    let ACocktail = document.createElement('article');
+    ACocktail.setAttribute("class", "card");
+
+    let cocktailName = _cocktail.strDrink;
+    ACocktail.innerHTML = '<img src="'+ _cocktail.strDrinkThumb + '"/><h5>' + cocktailName + '</h5>';
+    cardConteneur.appendChild(ACocktail);
+
+  });
+
 }
